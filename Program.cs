875d4728bac701afa40ad5e99bbd19ff57b36d24
Justin Hainline <Justin.Hainline@mssa.newhorizons.com>
2024-05-21 08:10:29 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Assignment_3._1._4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input the X coordinate: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Input the Y coordinate: ");
+            int y = Convert.ToInt32(Console.ReadLine());
+
+
+            if (x > 0 && y > 0)
+            {
+                Console.WriteLine($"The coordinate point ({x}, {y}) lies in the 1st quadrant.");
+            }
+            else if (x < 0 && y > 0)
+            {
+                Console.WriteLine($"The coordinate point ({x}, {y}) lies in the 2nd quadrant.");
+            }
+            else if (x < 0 && y < 0)
+            {
+                Console.WriteLine($"The coordinate point ({x}, {y}) lies in the 3rd quadrant.");
+            }
+            else if (x > 0 && y < 0)
+            {
+                Console.WriteLine($"The coordinate point ({x}, {y}) lies in the 4th quadrant.");
+            }
+            else if (x == 0 && y < 0)
+            {
+                Console.WriteLine($"The coordinate point ({x}, {y}) lies on the Y axis");
+            }
+            else if (x < 0 && y == 0)
+            {
+                Console.WriteLine($"The coordinate point ({x}, {y}) lies on the X axis");
+            }
+            else if (x == 0 && y == 0)
+            {
+                Console.WriteLine($"The coordinate point ({x}, {y}) lies at the origin.");
+            }
+        }
+    }
+}
